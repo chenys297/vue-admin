2c3b1c19b68f745bf09ace8dde45af3fa8375191
lip <eason.chen@panerg.com>
2020-07-30 18:06:50 +0800

    添加Icon组件

---
+++ src/components/Icon/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <i v-if="myIconfont" :class="['vue-admin-icon' , 'vue-admin-' + iconName]" :style="styles" />
+  <i v-else :class="iconName" :style="styles" />
+</template>
+
+<script>
+export default {
+  name: 'SvgIcon',
+  props: {
+    myIconfont: {
+      type: Boolean,
+      default: true
+    },
+    iconName: {
+      type: String,
+      required: true
+    },
+    fontSize: {
+      type: [String, Number],
+      default: 'inherit'
+    },
+    color: {
+      type: String,
+      default: 'inherit'
+    }
+  },
+  data () {
+    return {
+      styles: {}
+    }
+  },
+
+  watch: {
+    fontSize: {
+      handler: function (fontSize) {
+        if (fontSize) {
+          this.styles.fontSize = fontSize
+        } else {
+          this.styles.fontSize = 'inherit'
+        }
+      }
+    },
+
+    color: {
+      handler: function (color) {
+        if (color) {
+          this.styles.color = color
+        } else {
+          this.styles.color = 'inherit'
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+</style>
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+import Icon from './Icon/index.vue'
+
+Vue.component('Icon', Icon)
