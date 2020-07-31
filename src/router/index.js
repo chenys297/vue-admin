bdedfcda20ad0121f20487430fc2d8d3758abde0
lip <eason.chen@panerg.com>
2020-07-31 17:52:59 +0800

    修复布局异常问题

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,44 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Layout from '@/views/Layout/index.vue'
 
 Vue.use(VueRouter)
 
 const constRoutes = [
   {
     path: '/',
-    name: 'home',
+    name: 'layout',
+    component: Layout,
     redirect: '/dashoboard',
     hide: true,
     meta: {
       requireAuth: false,
-      title: '',
+      title: 'layout',
       icon: ''
-    }
+    },
+    children: [
+      {
+        path: '/dashoboard',
+        name: 'dashoboard',
+        component: () => import(/* webpackChunkName: "Dashboard" */ '@/views/Dashboard'),
+        hide: false,
+        meta: {
+          requireAuth: false,
+          title: 'dashboard',
+          icon: 'dashboard'
+        }
+      }
+    ]
   },
   {
-    path: '/dashoboard',
-    name: 'dashoboard',
-    component: () => import(/* webpackChunkName: "about" */ '@/views/Dashboard'),
-    hide: false,
+    path: '/login',
+    name: 'login',
+    component: () => import('@/views/Login'),
+    hidden: true,
     meta: {
       requireAuth: false,
-      title: 'dashboard',
-      icon: 'dashboard'
+      title: 'login',
+      icon: ''
     }
   }
 ]
