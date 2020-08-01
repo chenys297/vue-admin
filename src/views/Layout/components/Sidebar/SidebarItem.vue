e841484a9da336d940b6322abd74508974e34f3d
chenys297 <lip.smith297@gmail.com>
2020-08-01 21:36:56 +0800

    Icon组件属性名更改 iconName改为name

--- src/views/Layout/components/Sidebar/SidebarItem.vue
+++ src/views/Layout/components/Sidebar/SidebarItem.vue
@@ -5,14 +5,14 @@
     :route="route"
     @click="handleClickMenuItem(route)"
   >
-    <Icon v-if="route.meta.icon" :iconName="route.meta.icon" myIconfont style="margin-right: 5px;" />
+    <Icon v-if="route.meta.icon" :name="route.meta.icon" myIconfont style="margin-right: 5px;" />
     <span slot="title">{{ route.meta.title }}</span>
   </el-menu-item>
   <el-submenu v-else-if="route.hidden === false" :index="route.name">
     <template slot="title">
       <Icon
         v-if="route.meta.icon"
-        :iconName="route.meta.icon"
+        :name="route.meta.icon"
         myIconfont
         style="margin-right: 5px;"
       />
