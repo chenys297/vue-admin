20b0e2709b70bf2dbc664702ccc2960a1660872d
chenys297 <lip.smith297@gmail.com>
2020-08-01 23:09:06 +0800

    添加AppHeader 用户头像组件

--- src/views/Layout/components/AppHeader/index.vue
+++ src/views/Layout/components/AppHeader/index.vue
@@ -1,22 +1,28 @@
 <template>
-  <div class="df aic cpt-app-header">
-    <el-link :underline="false" @click="handleToggleCollapse">
-      <Icon :name="isCollapse ? 's-unfold' : 's-fold'" :myIconfont="false" size="36px" />
-    </el-link>
-    <el-breadcrumb separator-class="el-icon-arrow-right" style="margin-left: 20px;">
-      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-      <el-breadcrumb-item
-        v-for="route of matchRoutes"
-        :key="route.path"
-        :to="route.path"
-      >{{ route.meta.title }}</el-breadcrumb-item>
-    </el-breadcrumb>
+  <div class="df aic jcsb cpt-app-header">
+    <div class="df aic">
+      <el-link :underline="false" @click="handleToggleCollapse">
+        <Icon :name="isCollapse ? 's-unfold' : 's-fold'" :myIconfont="false" size="36px" />
+      </el-link>
+      <el-breadcrumb separator-class="el-icon-arrow-right" style="margin-left: 20px;">
+        <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+        <el-breadcrumb-item
+          v-for="route of matchRoutes"
+          :key="route.path"
+          :to="route.path"
+        >{{ route.meta.title }}</el-breadcrumb-item>
+      </el-breadcrumb>
+    </div>
+
+    <UserAvatar class="avatar" />
   </div>
 </template>
 
 <script>
+import UserAvatar from './userAvatar'
 export default {
   name: 'AppHeader',
+  components: { UserAvatar },
   data () {
     return {}
   },
@@ -46,5 +52,9 @@ export default {
   flex-grow: 1;
   min-height: 50px;
   padding: 0 5px;
+
+  .avatar {
+    float: right;
+  }
 }
 </style>
---
+++ src/views/Layout/components/AppHeader/userAvatar.vue
@@ -0,0 +1,39 @@
+<template>
+  <el-dropdown class="cpt-user-avatar" @command="handleCommand">
+    <span>
+      <el-avatar :src="avatarUrl" shape="circle" class="avatar" />
+      <Icon name="caret-bottom" :myIconfont="false" />
+    </span>
+    <el-dropdown-menu slot="dropdown">
+      <el-dropdown-item command="github">Github</el-dropdown-item>
+      <el-dropdown-item command="setting">设置</el-dropdown-item>
+      <el-dropdown-item command="logout" divided>退出登录</el-dropdown-item>
+    </el-dropdown-menu>
+  </el-dropdown>
+</template>
+
+<script>
+export default {
+  name: 'UserAvatar',
+  data () {
+    return {
+      avatarUrl: require('@/assets/images/avatar.jpg')
+    }
+  },
+
+  methods: {
+    handleCommand (command) {
+      console.log(command)
+    }
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+.cpt-user-avatar {
+  .avatar {
+    border: 1px solid #dddddd;
+    cursor: pointer;
+  }
+}
+</style>
